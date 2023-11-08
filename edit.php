0fad3f4b6ce5c6511927b12cffdc28981f1c1b22
Ntabethemba Mabetshe <bmaneza@gmail.com>
2023-11-08 08:07:38 +0000

    Updating the project making it usable for php8

--- edit.php
+++ edit.php
@@ -1,53 +1,64 @@
 <?php session_start();
-   require 'config.php';
-   if (isset($_GET['id'])) {
-      $data = $collection->findOne(['_id' => new MongoDB\BSON\ObjectID($_GET['id'])]);
-   }
-   if(isset($_POST['submit'])){
-      $collection->updateOne(
-         ['_id' => new MongoDB\BSON\ObjectID($_GET['id'])],
-         ['$set' => 
-           [
-          'name' => $_POST['name'],
-          'surname' => $_POST['surname'],
-          'idno' => $_POST['idno'],
-          'dob' => $_POST['dob'],
-           ]
+
+require 'config.php';
+if (isset($_GET['id'])) {
+   $data = $collection->findOne(['_id' => new MongoDB\BSON\ObjectID($_GET['id'])]);
+}
+
+if (isset($_POST['submit'])) {
+   $collection->updateOne(
+      ['_id' => new MongoDB\BSON\ObjectID($_GET['id'])],
+      [
+         '$set' =>
+         [
+            'name' => $_POST['name'],
+            'surname' => $_POST['surname'],
+            'idno' => $_POST['idno'],
+            'dob' => $_POST['dob'],
          ]
-      );
-      $_SESSION['success'] = "User updated Successfully!";
-      header("Location: index.php");
-   }
+      ]
+   );
+
+   $_SESSION['success'] = "User updated Successfully!";
+   header("Location: index.php");
+}
+
 ?>
 <!DOCTYPE html>
 <html>
-   <head>
-      <title>USERS</title>
-      <link rel="stylesheet" href="./vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
-   </head>
-   <body>
-      <div class="container">
-         <br>
-         <CENTER><h1>Edit Users</h1></CENTER>
-         <a href="index.php" class="btn btn-primary">Back</a>
-         <form method="POST">
-            <div class="form-group">
-               <strong>name:</strong>
-                <?php //$users = $collection->find();
-               //foreach ($users as $data) {}
-                  ?>
-               <input type="text"  value="<?php echo "$data->name"; ?>" class="form-control" name="name" required="" placeholder="Neslson">
-               <strong>surname:</strong>
-               <input type="text" required value="<?php echo "$data->surname"; ?>" class="form-control" name="surname" placeholder="surname Lengkap">
-               <strong>ID number:</strong>
-               <input type="number" value="<?php echo "$data->idno"; ?>" maxlength="13" class="form-control" name="idno" placeholder="xxxxxx xxxx xxx">
-               <strong>Date of birth:</strong>
-               <input type="date" value="<?php echo "$data->dob"; $dateInLocal; ?>"  date-date-format="YYYY/MM/DD" class="form-control" name="dob" placeholder="xxxx-xx-xx">
 
-               <br>
-               <button type="submit" name="submit" class="btn btn-success">Update</button>
-            </div>
-         </form>
-      </div>
-   </body>
+<head>
+   <title>USERS</title>
+   <link rel="stylesheet" href="./vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+   <div class="container">
+      <br>
+      <CENTER>
+         <h1>Edit Users</h1>
+      </CENTER>
+      <a href="index.php" class="btn btn-primary">Back</a>
+      <form method="POST">
+         <div class="form-group">
+            <strong>name:</strong>
+            <?php //$users = $collection->find();
+            //foreach ($users as $data) {}
+            ?>
+            <input type="text" value="<?php echo "$data->name"; ?>" class="form-control" name="name" required="" placeholder="Neslson">
+            <strong>surname:</strong>
+            <input type="text" required value="<?php echo "$data->surname"; ?>" class="form-control" name="surname" placeholder="surname Lengkap">
+            <strong>ID number:</strong>
+            <input type="number" value="<?php echo "$data->idno"; ?>" maxlength="13" class="form-control" name="idno" placeholder="xxxxxx xxxx xxx">
+            <strong>Date of birth:</strong>
+            <input type="date" value="<?php echo "$data->dob";
+                                       $dateInLocal; ?>" date-date-format="YYYY/MM/DD" class="form-control" name="dob" placeholder="xxxx-xx-xx">
+
+            <br>
+            <button type="submit" name="submit" class="btn btn-success">Update</button>
+         </div>
+      </form>
+   </div>
+</body>
+
 </html>
--- index.php
+++ index.php
@@ -1,49 +1,60 @@
 <?php session_start(); ?>
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>Infinity users</title>
-		<link rel="stylesheet" href="./vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
-	</head>
-	<body>
-		<div class="container">
-			<br>
-			<CENTER><h1>Infinity Users</h1></CENTER>
-			<a href="create.php" class="btn btn-success">Add</a>
-			<?php
-				if (isset($_SESSION['success'])) {
-					echo "<div class='alert alert-success'>".$_SESSION['success']."</div>";
-				}
-			?>
-			<table class="table">
-				<thead class="thead-dark">
-					<tr>
-						<th scope="col">Name</th>
-						<th scope="col">Surname</th>
-						<th scope="col">ID Number</th>
-						<th scope="col">Date of birth</th>
-						<th scope="col">Action</th>
-					</tr>
-				</thead>
-				<?php
-					require 'config.php';
-					$users = $collection->find();
-					foreach ($users as $data) {
 
-						echo "<tr>";
-						echo "<th scope='row'>".$data->name."</th>";
-						echo "<td>".$data->surname."</td>";
-						echo "<td>".$data->idno."</td>";
-						echo "<td>".$data->dob."</td>";
-						echo "<td>";
-						echo "<a href = 'edit.php?id=".$data->_id."'class='btn btn-primary'>EDIT</a>";
-						echo "<a href = 'delete.php?id=".$data->_id."'class='btn btn-danger'>DELETE</a>";
-						echo "</td>";
-						echo "</tr>";
-					
-				}
-				?>
-			</table>
-		</div>
-	</body>
+<head>
+	<title>Infinity users</title>
+	<link rel="stylesheet" href="./vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+	<div class="container">
+		<br>
+		<CENTER>
+			<h1>Infinity Users</h1>
+		</CENTER>
+		<a href="create.php" class="btn btn-success">Add</a>
+		<?php
+		if (isset($_SESSION['success'])) {
+			echo "<div class='alert alert-success'>" . $_SESSION['success'] . "</div>";
+		}
+		?>
+
+<?php
+		if (isset($_SESSION['error_message'])) {
+			echo "<div class='alert alert-error'>" . $_SESSION['error_message'] . "</div>";
+		}
+		?>
+
+		<table class="table">
+			<thead class="thead-dark">
+				<tr>
+					<th scope="col">Name</th>
+					<th scope="col">Surname</th>
+					<th scope="col">ID Number</th>
+					<th scope="col">Date of birth</th>
+					<th scope="col">Action</th>
+				</tr>
+			</thead>
+			<?php
+			require 'config.php';
+			$users = $collection->find();
+			foreach ($users as $data) {
+
+				echo "<tr>";
+				echo "<th scope='row'>" . $data->name . "</th>";
+				echo "<td>" . $data->surname . "</td>";
+				echo "<td>" . $data->idno . "</td>";
+				echo "<td>" . $data->dob . "</td>";
+				echo "<td>";
+				echo "<a href = 'edit.php?id=" . $data->_id . "'class='btn btn-primary'>EDIT</a>";
+				echo "<a href = 'delete.php?id=" . $data->_id . "'class='btn btn-danger'>DELETE</a>";
+				echo "</td>";
+				echo "</tr>";
+			}
+			?>
+		</table>
+	</div>
+</body>
+
 </html>
